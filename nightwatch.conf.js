fcd3f91b4563e67f50eba612473167e8f86573f8
aarkhipov <aarkhipov1991@gmail.com>
2017-11-22 06:03:58 +0400

    configure path to the geckodriver

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -13,7 +13,7 @@ module.exports = {
     "port": 4444,
     "cli_args": {
       "webdriver.chrome.driver": "",
-      "webdriver.gecko.driver": "",
+      "webdriver.gecko.driver": "./node_modules/webdriver-manager/selenium/geckodriver-v0.19.1.exe",
       "webdriver.edge.driver": ""
     }
   },
